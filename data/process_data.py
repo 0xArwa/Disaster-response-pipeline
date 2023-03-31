01d2319767462d51b2e633079cc92831fc84185f
Arwa Ali <arwaao+1@icloud.com>
2023-03-31 04:31:16 +0300

    ETL pipeline is modified

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,72 @@
+#importing libraries
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    
+    
+    """
+    #load the data into two variables
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    #merging the two datasets
+    df = messages.merge(categories, on = 'id')
+
+    return df
+
+
+def lastString(value):
+    return value[-1:]
+
 
 
 def clean_data(df):
-    pass
+    """
+    
+    
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # stroing column names in a list
+    category_colnames = [x for x in row]
+
+    # renaming the columns of `categories`
+    categories.columns = category_colnames
+
+    #loop through each column
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(np.str)
+        categories[column] = categories[column].map(lastString)
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(np.int64)
+
+    # drop the original (uncleaned) categories column from `df`
+    del df['categories']
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df2 = pd.concat([df, categories], axis=1)
+
+    #removing duplicates
+    df2 = df2.drop_duplicates()
+
+    return df2
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    
+    
+    """
+    engine = create_engine(database_filename)
+    df.to_sql('Responses', engine, index=False)
 
 
 def main():
